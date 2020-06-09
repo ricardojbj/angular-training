61abe14cf8099268c2baf3ecdef2b5f8f0ea97f8
Renier Perez <rainer991@gmail.com>
2020-06-08 21:21:24 -0400

    problema con boton y ocupacion

--- src/app/vista-principal/vista-principal.component.ts
+++ src/app/vista-principal/vista-principal.component.ts
@@ -64,7 +64,7 @@ export class VistaPrincipalComponent implements OnInit {
     });
 
 
-    this.form.valueChanges.subscribe( (data) => {
+    this.form.valueChanges.subscribe((data) => {
       this.formIsValid = this.form.valid;
     });
 
